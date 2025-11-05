2e1a3cca514dfc5230be7c3f5d92831b93d0ff85
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-11-05 08:31:42 +0100

    reduce quiets more
    bench: 10932132

--- engine/search.cpp
+++ engine/search.cpp
@@ -505,6 +505,10 @@ Value __recurse(Board &board, int depth, Value alpha = -VALUE_INFINITE, Value be
 		if (depth >= 2 && i >= 1 + 2 * pv) {
 			Value r = reduction[i][depth];
 
+			if (!capt) {
+				r += 512;
+			}
+
 			r -= 1024 * pv;
 			r += 1024 * (!pv && cutnode);
 			if (move == line[ply].killer[0] || move == line[ply].killer[1])
